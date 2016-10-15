8a5b5b753f9d58b15214d7df6f45f642411a457a
Maxislav <lmaxim@mail.ru>
2016-10-15 20:43:35 +0300

    sds

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 var fs = require('fs');
 let history = [];
 let srcPath = '';
+let steps = 20;
+let backUpDir = "./autobackup";
+
   process.argv.forEach((val, index, array)=> {
     if (2 <= index) {
       srcPath += val + ' '
@@ -13,14 +16,31 @@ srcPath = srcPath.replace(/\s+$/g, "");
   console.log(srcPath);
 
 fs.watchFile(srcPath, function (e) {
-  //console.log(e)
-  let fileNameBackUp = "autobackup_"+history.length+"_"+srcPath;
-  history.push(fileNameBackUp);
-  console.log(fileNameBackUp);
-	fs.createReadStream(srcPath).pipe(fs.createWriteStream(fileNameBackUp));
+  if (!fs.existsSync(backUpDir)){
+    fs.mkdirSync(backUpDir);
+  }
+
+  let fileNameBackUp = "autobackup_"+0+"_"+srcPath;
+  history = (fs.readdirSync(backUpDir ));
 
 
-
+  if(history.length){
+    let arrNewName = [];
+    history.reverse();
+    history.forEach(fileName=>{
+      let newName =  fileName.replace(/_\d+_/, function(r){
+        let n = parseInt(r.replace(/_/g, ""));
+        n++;
+        return "_"+n+"_"
+      });
+      fs.renameSync( backUpDir +"/"+fileName,  backUpDir +"/"+newName);
+    });
+  }
+  fs.createReadStream(srcPath).pipe(fs.createWriteStream(backUpDir+"/"+fileNameBackUp));
+  if(steps<history.length){
+    fs.unlinkSync(backUpDir+"/"+history[history.length-1])
+  }
 
 });
+
 
